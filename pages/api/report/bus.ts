8e65048187f5a329406f17cc165a1bff14490070
cbarkr <cbarkr@gmail.com>
2023-10-18 22:28:52 -0700

    implement DB connection and reporting API routes

--- pages/api/report/bus.ts
+++ pages/api/report/bus.ts
@@ -1,39 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-import { Bus } from '@/models/reports/bus'
+import { Bus } from "@/models/reports/bus";
 
-
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION
+});
 const docClient = DynamoDBDocumentClient.from(client);
-
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const busReport = new Bus(
       req.body.stopID,
       req.body.busID,
       req.body.routeID,
       req.body.personCount
-    )
-
-    console.log(busReport)
+    );
 
     const command = new PutCommand({
-      TableName: process.env.BUS_TABLE_NAME,
-      Item: {
-        // TODO: Insert busReport
-      },
+      TableName: process.env.AWS_BUS_TABLE_NAME,
+      Item: busReport
     });
-  
-    const response = await docClient.send(command);
 
-    console.log(response)
-
-    res.status(201).json(busReport)
+    await docClient.send(command);
+    res.status(201).json(busReport);
   }
 }
--- pages/api/report/stop.ts
+++ pages/api/report/stop.ts
@@ -1,37 +1,27 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-import { Stop } from '@/models/reports/stop'
+import { Stop } from "@/models/reports/stop";
 
-
-const client = new DynamoDBClient({});
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION
+});
 const docClient = DynamoDBDocumentClient.from(client);
-
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    const stopReport = new Stop(
-      req.body.stopID,
-      req.body.personCount
-    )
-
-    console.log(stopReport)
+  if (req.method === "POST") {
+    const stopReport = new Stop(req.body.stopID, req.body.personCount);
 
     const command = new PutCommand({
-      TableName: process.env.STOP_TABLE_NAME,
-      Item: {
-        // TODO: Insert stopReport
-      },
+      TableName: process.env.AWS_STOP_TABLE_NAME,
+      Item: stopReport
     });
-  
-    const response = await docClient.send(command);
 
-    console.log(response)
-
-    res.status(201).json(stopReport)
+    await docClient.send(command);
+    res.status(201).json(stopReport);
   }
 }
