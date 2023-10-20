abe35f5647a4f29144a00b8318e31dd6da5e8503
cbarkr <cbarkr@gmail.com>
2023-10-20 14:55:34 -0700

    `stop` should be type `IStopDetails`

--- app/page.tsx
+++ app/page.tsx
@@ -4,46 +4,61 @@ import { useState } from 'react';
 import React from 'react';
 import dynamic from 'next/dynamic';
 
+import { IStopDetails } from '@/interfaces/stopdetails';
+
 
 const StopSearch = dynamic(() => import('./components/StopSearch'))
 const StopList = dynamic(() => import('./components/StopList'))
-const StopDetails = dynamic(() => import('./components/StopDetails'))
+const StopSelected = dynamic(() => import('./components/StopSelected'))
 
 
 export const StopContext = React.createContext({
-  stop: {},
-  setStop: (stop: {}) => {},
-  stops: {},
+  setStop: (stop: IStopDetails) => {},
   setStops: (stops: {}) => {}
 })
 
 export const StopListContext = React.createContext({
-  stop: {},
-  setStop: (stop: {}) => {}
+  setStop: (stop: IStopDetails) => {}
 })
 
 
-const isEmpty = (obj: object) => {
+const isObjectEmpty = (obj: object) => {
   return Object.keys(obj).length === 0;
+}
+
+const isStopEmpty = (stop: IStopDetails) => {
+  return Number.isNaN(stop.StopNo)
 }
 
 
 export default function Home() {
-  const [stop, setStop] = useState({})
+  const [stop, setStop] = useState<IStopDetails>({
+    StopNo: NaN,
+    Name: '',
+    BayNo: '',
+    City: '',
+    OnStreet: '',
+    AtStreet: '',
+    Latitude: NaN,
+    Longitude: NaN,
+    WheelchairAccess: NaN,
+    Distance: NaN,
+    Routes: '',
+  })
   const [stops, setStops] = useState({})
   
 
   return (
     <div className='w-full p-24'>
       <div className='flex flex-col items-center'>
-        <StopContext.Provider value={{ stop: stop, stops: stops, setStop: setStop, setStops: setStops }}>
+        <StopContext.Provider value={{ setStop: setStop, setStops: setStops }}>
           <StopSearch />
         </StopContext.Provider>
-        { !isEmpty(stop) && <StopDetails stop={ stop } /> }
-        { (isEmpty(stop) && isEmpty(stops)) && <div>Select a stop to get started</div> }
+        { !isStopEmpty(stop) && <StopSelected stop={ stop } /> }
+        { (isStopEmpty(stop) && isObjectEmpty(stops)) && <div>Select a stop to get started</div> }
         {
-          (isEmpty(stop) && !(isEmpty(stops))) && (
-            <StopListContext.Provider value={{ stop: stop, setStop: setStop }}>
+          (isStopEmpty(stop) && !(isObjectEmpty(stops))) && (
+            <StopListContext.Provider value={{ setStop: setStop }}>
               <StopList stops={stops} />
             </StopListContext.Provider>
           )
