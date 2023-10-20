60c729d5c6ec7e1a25088ee76e40cf187c5cc31e
cbarkr <cbarkr@gmail.com>
2023-10-20 14:55:47 -0700

    implement `StopDetails`

--- app/components/StopDetails.tsx
+++ app/components/StopDetails.tsx
@@ -1,6 +1,33 @@
-export default function StopDetails({ stop }) {
-    return (
-        // TODO
-        <div></div>
-    )
+import { IStopDetails } from "@/interfaces/stopdetails";
+
+
+interface StopDetailsProps {
+  stop: IStopDetails;
+}
+
+
+export default function StopDetails({ stop }: StopDetailsProps) {
+  return (
+    <div
+      className="rounded-lg p-2 bg-gray-200"
+    >
+      <div className="flex flex-row justify-between">
+        <div className="font-bold">{stop.StopNo}</div>
+        <div className="mx-20 lg:mx-24" />
+        <div>
+          {stop.OnStreet} & {stop.AtStreet}
+        </div>
+      </div>
+      <div className="flex flex-row">
+        {stop.Routes.split(",").map((route: string) => (
+          <div
+            className="rounded-full p-2 font-bold bg-purple-400 text-amber-950"
+            key={route}
+          >
+            {route ? route : "N/A"}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
