f64625960239f80fdfe1e76a84c6732662d6a80d
cbarkr <cbarkr@gmail.com>
2023-10-21 11:08:54 -0700

    add header to `_app.tsx` + make header title link home

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className="w-full h-24 sticky top-0">
       <div className="flex flex-row justify-between items-center px-8 py-2">
-        <div className="text-lg">Transmiss</div>
+        <Link href="/" className="text-lg hover:text-purple-400">
+          Transmiss
+        </Link>
         <Link href="/about" className="text-lg hover:text-purple-400">
           About
         </Link>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from "next/app";
 import "../app/styles/globals.css";
 
+import Header from "@/app/components/header";
+
 interface CustomPageProps {}
 
 export default function App({
   Component,
   pageProps,
 }: AppProps<CustomPageProps>) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
