2a335126efa3bd2f13a5097857f27f1dec0c345a
cbarkr <cbarkr@gmail.com>
2023-10-21 17:42:07 -0700

    visual confirmation upon form submit + state bug fix

--- app/components/stopSelected.tsx
+++ app/components/stopSelected.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Slider } from "@mui/material";
-import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import { CheckCircle } from "@mui/icons-material";
 import dynamic from "next/dynamic";
 
 import { IStopDetails } from "@/interfaces/stopdetails";
@@ -18,8 +17,25 @@ export default function StopSelected({ stop }: StopSelectedProps) {
   // NOTE: crowded and full should be mutually exclusive
   const [crowded, setCrowded] = useState(false);
   const [full, setFull] = useState(false);
-
+  const [submitted, setSubmitted] = useState(false);
   const [numPeople, setNumPeople] = useState(0);
+
+  const mounted = useRef(0);
+
+  useEffect(() => {
+    if (!mounted.current) {
+      mounted.current = stop.StopNo;
+    } else {
+      if (mounted.current !== stop.StopNo) {
+        // Reset if new stop has been selected
+        mounted.current = stop.StopNo;
+        setFull(false);
+        setCrowded(false);
+        setSubmitted(false);
+        setNumPeople(0);
+      }
+    }
+  });
 
   const reportCrowded = () => {
     setFull(false);
@@ -47,13 +63,16 @@ export default function StopSelected({ stop }: StopSelectedProps) {
     axios
       .post("/api/report/stop", {
         stopID: stop.StopNo,
-        personCount: numPeople
+        personCount: numPeople,
       })
       .then((res: any) => {
-        console.log(res)
+        console.log(res);
       })
       .catch((err: any) => {
         console.error(err);
+      })
+      .finally(() => {
+        setSubmitted(true);
       });
   };
 
@@ -68,10 +87,13 @@ export default function StopSelected({ stop }: StopSelectedProps) {
         // routeID: 0,
       })
       .then((res: any) => {
-        console.log(res)
+        console.log(res);
       })
       .catch((err: any) => {
         console.error(err);
+      })
+      .finally(() => {
+        setSubmitted(true);
       });
   };
 
@@ -79,79 +101,87 @@ export default function StopSelected({ stop }: StopSelectedProps) {
     <div className="m-1 rounded-lg p-2 bg-gray-200">
       <StopDetails stop={stop} />
       <p className="font-bold my-4">Report</p>
-      {
-        (!crowded && !full) && (
-          <div className="flex flex-row justify-between">
+      {!crowded && !full && (
+        <div className="flex flex-row justify-between">
+          <button
+            onClick={reportCrowded}
+            type="button"
+            className="rounded-full p-2 bg-white border-solid border-2 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            Crowded stop
+          </button>
+          <button
+            onClick={reportFull}
+            type="button"
+            className="rounded-full py-2 px-8 bg-amber-800 text-white border-solid border-2 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          >
+            Full bus
+          </button>
+        </div>
+      )}
+      {crowded && (
+        <>
+          <p>How many people are at this stop?</p>
+          <div className="flex flex-row justify-between items-center">
+            {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
+            <Slider
+              valueLabelDisplay="auto"
+              color="info"
+              value={numPeople}
+              onChange={handleChange}
+              defaultValue={5}
+              step={1}
+              min={1}
+              max={20}
+              className="mx-4"
+            />
+          </div>
+          <div className="flex flex-col items-center my-4">
             <button
-              onClick={reportCrowded}
               type="button"
-              className="rounded-full p-2 bg-white border-solid border-2 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              disabled={submitted}
+              onClick={handleCrowdedSubmit}
+              className="rounded-full py-2 px-8 bg-emerald-500 text-white transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
             >
-              Crowded stop
-            </button>
-            <button
-              onClick={reportFull}
-              type="button"
-              className="rounded-full py-2 px-8 bg-amber-800 text-white border-solid border-2 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            >
-              Full bus
+              {
+                submitted ? <CheckCircle /> : <p>Submit</p>
+              }
             </button>
           </div>
-        )
-      }
-      {
-        crowded && (
-          <>
-            <p>How many people are at this stop?</p>
-            <div className="flex flex-row justify-between items-center">
-              {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
-              <SentimentSatisfiedAltIcon />
-              <Slider 
-                valueLabelDisplay="auto"
-                color="info"
-                value={numPeople} 
-                onChange={handleChange}
-                defaultValue={5}
-                step={1}
-                min={1}
-                max={20}
-                className="mx-4"
-              />
-              <SentimentVeryDissatisfiedIcon />
-            </div>
-            <div className="flex flex-col items-center my-4">
-              <button type="button" onClick={handleCrowdedSubmit} className="rounded-full py-2 px-8 bg-emerald-500 text-white transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Submit</button>
-            </div>
-          </>
-        )
-      }
-      {
-        full && (
-          <>
-            <p>How many people are at this stop? (optional)</p>
-            <div className="flex flex-row justify-between items-center">
-              {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
-              <SentimentSatisfiedAltIcon />
-              <Slider 
-                valueLabelDisplay="auto"
-                color="info"
-                value={numPeople} 
-                onChange={handleChange}
-                defaultValue={5}
-                step={1}
-                min={1}
-                max={20}
-                className="mx-4"
-              />
-              <SentimentVeryDissatisfiedIcon />
-            </div>
-            {/* TODO: Input for bus ID and route number? */}
-            <div className="flex flex-col items-center my-4">
-              <button type="button" onClick={handleFullSubmit} className="rounded-full py-2 px-8 bg-emerald-500 text-white transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">Submit</button>
-            </div>
-          </>
-        )
-      }
+        </>
+      )}
+      {full && (
+        <>
+          <p>How many people are at this stop? (optional)</p>
+          <div className="flex flex-row justify-between items-center">
+            {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
+            <Slider
+              valueLabelDisplay="auto"
+              color="info"
+              value={numPeople}
+              onChange={handleChange}
+              defaultValue={5}
+              step={1}
+              min={1}
+              max={20}
+              className="mx-4"
+            />
+          </div>
+          {/* TODO: Input for bus ID and route number? */}
+          <div className="flex flex-col items-center my-4">
+            <button
+              type="button"
+              disabled={submitted}
+              onClick={handleFullSubmit}
+              className="rounded-full py-2 px-8 bg-emerald-500 text-white transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+            >
+              {
+                submitted ? <CheckCircle /> : <p>Submit</p>
+              }
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
