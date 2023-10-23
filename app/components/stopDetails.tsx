b4e32ee23f71b5fb9943071ca8ed89871d806142
cbarkr <cbarkr@gmail.com>
2023-10-22 19:06:47 -0700

    slightly rework stop detail view

--- app/components/stopDetails.tsx
+++ app/components/stopDetails.tsx
@@ -6,23 +6,21 @@ interface StopDetailsProps {
 
 export default function StopDetails({ stop }: StopDetailsProps) {
   return (
-    <div className="rounded-lg p-2 bg-gray-200 max-w-screen-sm">
-      <div className="flex flex-row justify-between">
+    <div className="rounded-lg p-2 bg-neutral-200 max-w-screen-sm">
+      <div className="flex flex-row justify-between my-2">
         <p className="text-l md:text-xl font-bold">{stop.StopNo}</p>
-        <p>
-          {stop.OnStreet} & {stop.AtStreet}
-        </p>
+        <div className="flex flex-row">
+          {stop.Routes.split(",").map((route: string) => (
+            <div
+              className="rounded-lg p-2 font-bold bg-secondary-300 text-primary-950"
+              key={route}
+            >
+              {route ? route : "N/A"}
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="flex flex-row">
-        {stop.Routes.split(",").map((route: string) => (
-          <div
-            className="rounded-full p-2 font-bold bg-purple-400 text-amber-950"
-            key={route}
-          >
-            {route ? route : "N/A"}
-          </div>
-        ))}
-      </div>
+      <p>{stop.OnStreet} & {stop.AtStreet}</p>
     </div>
   );
 }
