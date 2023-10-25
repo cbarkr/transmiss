485039f5564c90d76443ea4670d280dd63cc0d0d
cbarkr <cbarkr@gmail.com>
2023-10-24 22:10:56 -0700

    add no-show report

--- app/components/stopDetails.tsx
+++ app/components/stopDetails.tsx
@@ -12,7 +12,7 @@ export default function StopDetails({ stop }: StopDetailsProps) {
         <div className="flex flex-row">
           {stop.Routes.split(",").map((route: string) => (
             <div
-              className="rounded-lg p-2 font-bold bg-secondary-300 text-primary-950"
+              className="rounded-full py-2 px-4 font-bold bg-secondary-300 text-sm text-primary-950"
               key={route}
             >
               {route ? route : "N/A"}
