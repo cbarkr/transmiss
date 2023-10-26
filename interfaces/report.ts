feecf6c0d7e11aaae7bd1e9e3af1b2cbfafd72ed
cbarkr <cbarkr@gmail.com>
2023-10-25 18:10:46 -0700

    HOT FIX: Update schema

--- interfaces/report.ts
+++ interfaces/report.ts
@@ -2,6 +2,6 @@ export interface IReport {
   readonly stop_id: number;
   readonly report_type: string;
   readonly bus_id?: number;
-  readonly route_id?: number;
+  readonly route_id?: string;
   readonly person_count?: number;
 };
--- models/report.ts
+++ models/report.ts
@@ -4,16 +4,16 @@ import { IReport } from "@/interfaces/report";
 
 export class Report implements IReport {
   report_id: string;
-  datetime: string;
+  report_datetime: string;
   stop_id: number;
   report_type: string;
   bus_id?: number;
-  route_id?: number;
+  route_id?: string;
   person_count?: number;
 
   constructor(params: IReport) {
     this.report_id = randomUUID();
-    this.datetime = new Date().toISOString();
+    this.report_datetime = new Date().toISOString();
     this.stop_id = params.stop_id;
     this.report_type = params.report_type;
     this.bus_id = params.bus_id;
