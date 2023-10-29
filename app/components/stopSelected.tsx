456ef93b553cfdcf0a9f6c9c93b3fde7df190001
cbarkr <cbarkr@gmail.com>
2023-10-28 18:27:57 -0700

    move repetitive markup into components + improvements

--- app/components/stopSelected.tsx
+++ app/components/stopSelected.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { CheckCircle } from "@mui/icons-material";
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import dynamic from "next/dynamic";
 
 import { usePrevious } from "../hooks/prev";
@@ -10,11 +11,99 @@ const StopDetails = dynamic(() => import("./stopDetails"));
 
 const axios = require("axios").default;
 
-interface StopSelectedProps {
+interface IPeopleCounterProps {
+  currNum: number;
+  handler: (newNum: number) => void;
+  disabled: boolean;
+}
+
+interface IRouteSelectorProps {
+  routes: string;
+  handler: (newRouteID: string) => void;
+  curr: string;
+}
+
+interface ISubmitButtonProps {
+  submitted: boolean;
+  handler: () => void;
+}
+
+interface IStopSelectedProps {
   stop: IStopDetails;
 }
 
-export default function StopSelected({ stop }: StopSelectedProps) {
+function PeopleCounter({currNum, handler, disabled}: IPeopleCounterProps) {
+  const handleChange = (e: any) => {
+    const newNum = Number.parseInt(e)
+    handler(newNum > 1 ? newNum : currNum)
+  }
+
+  return (
+    <div className="flex flex-row items-center rounded-full text-secondary-300 bg-primary-950 dark:text-primary-950 dark:bg-secondary-300 w-fit">
+      {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
+      <button
+        type="button"
+        disabled={disabled}
+        onClick={() => handler(currNum > 1 ? currNum - 1 : 0)}
+        className="rounded-full py-2 px-4 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      >
+        -
+      </button>
+      <input
+        onChange={(e) => handleChange(e.target.value)}
+        value={currNum}
+        type="number"
+        size={1}
+        className="rounded-full bg-transparent outline-none text-center"
+      ></input>
+      <button 
+        type="button"
+        disabled={disabled}
+        onClick={() => handler(currNum + 1)}
+        className="rounded-full py-2 px-4 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      >
+        +
+      </button>
+    </div>
+  )
+}
+
+function RouteSelector({routes, handler, curr}: IRouteSelectorProps) {
+  return (
+    <div className="flex flex-row items-center">
+      {routes.split(",").map((route: string) => (
+        <button
+          key={route}
+          type="button"
+          disabled={curr === route}
+          onClick={() => handler(route)}
+          className="rounded-lg py-2 px-4 font-bold bg-secondary-300 text-primary-950 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+        >
+          {route ? route : "N/A"}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+function SubmitButton({submitted, handler}: ISubmitButtonProps) {
+  return (
+    <div className="flex flex-col items-center my-2">
+      <button
+        type="button"
+        disabled={submitted}
+        onClick={handler}
+        className="rounded-full py-2 px-8 bg-success-600 text-success-50 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+      >
+        {
+          submitted ? <CheckCircle /> : <p>Submit</p>
+        }
+      </button>
+    </div>
+  )
+}
+
+export default function StopSelected({ stop }: IStopSelectedProps) {
   // NOTE: crowded and full should be mutually exclusive
   const [crowded, setCrowded] = useState(false);
   const [full, setFull] = useState(false);
@@ -43,6 +132,14 @@ export default function StopSelected({ stop }: StopSelectedProps) {
       }
     }
   }, [stop, previousStop, reports]);
+
+  const updateNumPeople = (newNum: number) => {
+    setNumPeople(newNum);
+  }
+
+  const updateRouteID = (newRouteID: string) => {
+    setRouteID(newRouteID);
+  }
 
   const reportCrowded = () => {
     setFull(false);
@@ -109,9 +206,19 @@ export default function StopSelected({ stop }: StopSelectedProps) {
   };
 
   return (
-    <div className="m-1 rounded-lg p-2 bg-neutral-200 dark:bg-neutral-900">
-      <StopDetails stop={stop} reports={reports}/>
-      <div className="p-2 max-w-screen-sm">
+    <div className="m-2">
+      {
+        reports && (
+          <div className="flex flex-row items-center rounded-lg my-1 p-2 bg-flush-orange-950">
+            {/* Magic strings are evil but MUI and tailwind don't play nicely together */}
+            {/* #381717 is error-950 */}
+            <ErrorOutlineIcon sx={{ color: 'white' }} />
+            <p className="mx-2 text-white">Users have reported issues with this stop within the last hour. Submit a report to help others!</p>
+          </div>
+        )
+      }
+      <StopDetails stop={stop} />
+      <div className="rounded-lg my-1 p-2 max-w-screen-sm bg-primary-200 dark:bg-primary-950">
         <p className="font-bold my-2">Report</p>
         {!crowded && !full && !noShow && (
           <div className="flex flex-row justify-center">
@@ -141,160 +248,34 @@ export default function StopSelected({ stop }: StopSelectedProps) {
         {crowded && (
           <>
             <p>How many people are at this stop?</p>
-            <div className="flex flex-row justify-between items-center">
-              {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
-              <div>
-                <div className="rounded-lg py-2 px-4 bg-primary-950 text-secondary-300">{numPeople}</div>
-              </div>
-              <div>
-                <button 
-                  type="button"
-                  disabled={submitted}
-                  onClick={() => setNumPeople(numPeople => numPeople > 1 ? numPeople - 1 : 0)}
-                  className="rounded-full py-2 px-4 bg-primary-950 text-secondary-300 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                >
-                  -
-                </button>
-                <button 
-                  type="button"
-                  disabled={submitted}
-                  onClick={() => setNumPeople(numPeople => numPeople + 1)}
-                  className="rounded-full py-2 px-4 bg-primary-950 text-secondary-300 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                >
-                  +
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-col items-center my-2">
-              <button
-                type="button"
-                disabled={submitted}
-                onClick={handleCrowdedSubmit}
-                className="rounded-full py-2 px-8 bg-success-600 text-success-50 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              >
-                {
-                  submitted ? <CheckCircle /> : <p>Submit</p>
-                }
-              </button>
-            </div>
+            <PeopleCounter currNum={numPeople} handler={updateNumPeople} disabled={submitted} />
+            <SubmitButton submitted={submitted} handler={handleCrowdedSubmit} />
           </>
         )}
         {noShow && (
           <>
             <div className='my-2'>
               <p>Which bus passed you?</p>
-              <div className="flex flex-row items-center">
-                {stop.Routes.split(",").map((route: string) => (
-                  <button
-                    key={route}
-                    type="button"
-                    disabled={routeID === route}
-                    onClick={() => setRouteID(route)}
-                    className="rounded-lg py-2 px-4 font-bold bg-secondary-300 text-primary-950 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  >
-                    {route ? route : "N/A"}
-                  </button>
-                ))}
-              </div>
+              <RouteSelector routes={stop.Routes} handler={updateRouteID} curr={routeID} />
             </div>
             <div className='my-2'>
               <p>How many people are at this stop? (optional)</p>
-              <div className="flex flex-row justify-between items-center">
-                {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
-                <div>
-                  <div className="rounded-lg py-2 px-4 bg-primary-950 text-secondary-300">{numPeople}</div>
-                </div>
-                <div>
-                  <button 
-                    type="button"
-                    disabled={submitted}
-                    onClick={() => setNumPeople(numPeople => numPeople > 1 ? numPeople - 1 : 0)}
-                    className="rounded-full py-2 px-4 bg-primary-950 text-secondary-300 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  >
-                    -
-                  </button>
-                  <button 
-                    type="button"
-                    disabled={submitted}
-                    onClick={() => setNumPeople(numPeople => numPeople + 1)}
-                    className="rounded-full py-2 px-4 bg-primary-950 text-secondary-300 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
+              <PeopleCounter currNum={numPeople} handler={updateNumPeople} disabled={submitted} />
             </div>
-            <div className="flex flex-col items-center my-2">
-              <button
-                type="button"
-                disabled={submitted}
-                onClick={handleNoShowSubmit}
-                className="rounded-full py-2 px-8 bg-success-600 text-success-50 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              >
-                {
-                  submitted ? <CheckCircle /> : <p>Submit</p>
-                }
-              </button>
-            </div>
+            <SubmitButton submitted={submitted} handler={handleNoShowSubmit} />
           </>
         )}
         {full && (
           <>
             <div className='my-2'>
               <p>Which bus passed you?</p>
-              <div className="flex flex-row items-center">
-                {stop.Routes.split(",").map((route: string) => (
-                  <button
-                    key={route}
-                    type="button"
-                    disabled={routeID === route}
-                    onClick={() => setRouteID(route)}
-                    className="rounded-lg py-2 px-4 font-bold bg-secondary-300 text-primary-950 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  >
-                    {route ? route : "N/A"}
-                  </button>
-                ))}
-              </div>
+              <RouteSelector routes={stop.Routes} handler={updateRouteID} curr={routeID} />
             </div>
             <div className='my-2'>
               <p>How many people are at this stop? (optional)</p>
-              <div className="flex flex-row justify-between items-center">
-                {/* TODO: Consider getting estimate instead? Like https://transitapp.com/rats */}
-                <div>
-                  <div className="rounded-lg py-2 px-4 bg-primary-950 text-secondary-300">{numPeople}</div>
-                </div>
-                <div>
-                  <button 
-                    type="button"
-                    disabled={submitted}
-                    onClick={() => setNumPeople(numPeople => numPeople > 1 ? numPeople - 1 : 0)}
-                    className="rounded-full py-2 px-4 bg-primary-950 text-secondary-300 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  >
-                    -
-                  </button>
-                  <button 
-                    type="button"
-                    disabled={submitted}
-                    onClick={() => setNumPeople(numPeople => numPeople + 1)}
-                    className="rounded-full py-2 px-4 bg-primary-950 text-secondary-300 text-xl transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
+              <PeopleCounter currNum={numPeople} handler={updateNumPeople} disabled={submitted} />
             </div>
-            <div className="flex flex-col items-center my-2">
-              <button
-                type="button"
-                disabled={submitted}
-                onClick={handleFullSubmit}
-                className="rounded-full py-2 px-8 bg-success-600 text-success-50 transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              >
-                {
-                  submitted ? <CheckCircle /> : <p>Submit</p>
-                }
-              </button>
-            </div>
+            <SubmitButton submitted={submitted} handler={handleFullSubmit} />
           </>
         )}
       </div>
