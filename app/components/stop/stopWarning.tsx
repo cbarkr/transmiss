e48a317412e071daf8a497248b221cec7d4d453e
cbarkr <cbarkr@gmail.com>
2023-11-05 13:33:38 -0800

    shorten stop warning text

--- app/components/stop/stopWarning.tsx
+++ app/components/stop/stopWarning.tsx
@@ -5,8 +5,8 @@ export default function StopWarning() {
     <div className="flex flex-row items-center rounded-lg my-1 p-2 bg-yellows-200/80">
       <ErrorOutlineIcon className="text-yellows-900" />
       <div className="mx-2 text-yellows-900">
-        <p className="text-lg font-bold">Issues have been reported within the last hour</p>
-        <p>Submit another report to help!</p>
+        <p className="text-lg font-bold">Issues reported within the past hour</p>
+        <p>Submit a report to help!</p>
       </div>
     </div>
   );
