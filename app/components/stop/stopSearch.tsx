76c67cba10aaaec1d02ef8393843e4abf2e2d89b
cbarkr <cbarkr@gmail.com>
2023-12-06 21:58:30 -0800

    temporarily remove search by location now that translink is acting up :(

--- app/components/stop/stopSearch.tsx
+++ app/components/stop/stopSearch.tsx
@@ -114,7 +114,7 @@ export default function StopSearch() {
     <div className="w-full">
       <form onSubmit={(e) => handleSubmit(e)}>
         <div className="flex flex-row justify-between items-center">
-          <div className="flex flex-row w-full py-2 items-center rounded-full border-solid border-2 dark:border-1 border-primary-950 dark:border-white bg-transparent">
+          <div className="flex flex-row w-full py-2 items-center rounded-3xl bg-white text-black">
             <input
               onChange={(e) => handleChange(e.target.value)}
               value={stopID}
@@ -124,7 +124,7 @@ export default function StopSearch() {
               type="text"
               inputMode="decimal"
               placeholder="Search by stop number"
-              className="w-full rounded-3xl m-2 pl-2 text-primary-950 dark:text-primary-50 bg-transparent outline-none"
+              className="w-full rounded-3xl m-2 pl-2 bg-transparent outline-none text-black"
             ></input>
             <button
               type="submit"
@@ -133,13 +133,13 @@ export default function StopSearch() {
               <SearchIcon />
             </button>
           </div>
-          <button
+          {/* <button
             onClick={getCoords}
             type="button"
             className="rounded-full m-2 p-2 border-solid border-2 dark:border-1 border-primary-950 dark:border-white bg-transparent transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-75 disabled:shadow-none"
           >
             {locationInUse ? <GpsFixedIcon /> : <GpsNotFixedIcon />}
-          </button>
+          </button> */}
         </div>
       </form>
     </div>
