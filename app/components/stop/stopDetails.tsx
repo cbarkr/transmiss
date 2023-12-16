9f10ea4c241af366caa237e67e8d576012434d61
cbarkr <cbarkr@gmail.com>
2023-12-15 18:44:26 -0800

    remove translink link, change report button

--- app/components/stop/stopDetails.tsx
+++ app/components/stop/stopDetails.tsx
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 
 import { IStopDetails } from "@/interfaces/stop";
 import PlaceIcon from "@mui/icons-material/Place";
-import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const StopWarning = dynamic(() => import("./stopWarning"));
 
@@ -49,39 +49,19 @@ export default function StopDetails({
             </div>
           )}
         </div>
+        {selected && !showReportMenu && (
+          <div className="flex flex-row mt-1 gap-1">
+            <button
+              onClick={handleShowReportMenu}
+              type="button"
+              className="flex flex-col min-w-[5rem] w-[10dvh] min-h-[5rem] h-[10dvh] justify-center items-center text-center text-xl rounded-full bg-primary-200 text-black transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-75 disabled:shadow-none"
+            >
+              <div>Report</div>
+              <ArrowForwardIcon />
+            </button>
+          </div>
+        )}
       </div>
-      {selected && !showReportMenu && (
-        <div className="flex flex-row mt-1 gap-1">
-          <button
-            onClick={handleShowReportMenu}
-            type="button"
-            className="flex flex-col text-xl basis-1/2 rounded-t-3xl rounded-r-3xl px-2 py-6 bg-primary-200 text-black transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-75 disabled:shadow-none"
-          >
-            <div className="flex flex-row h-full justify-center items-center text-center ">
-              Report here
-            </div>
-            <div className="flex flex-row justify-start">
-              <div className="rotate-180">
-                <ArrowOutwardIcon fontSize="large" />
-              </div>
-            </div>
-          </button>
-          <a
-            href="https://www.translink.ca/about-us/customer-service/feedback"
-            target="_blank"
-            rel="noopener noreferrer"
-            role="button"
-            className="flex flex-col text-xl basis-1/2 rounded-b-3xl rounded-l-3xl px-2 py-4 bg-primary-200 text-black transition-all hover:shadow-lg focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-75 disabled:shadow-none"
-          >
-            <div className="flex flex-row justify-end">
-              <ArrowOutwardIcon fontSize="large" />
-            </div>
-            <div className="flex flex-row h-full justify-center items-center text-center ">
-              Report to TransLink
-            </div>
-          </a>
-        </div>
-      )}
     </>
   );
 }
