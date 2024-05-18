2fcfb6b011d982620dc852b63210a081224a2c66
cbarkr <cbarkr@gmail.com>
2024-05-18 13:39:07 -0700

    distinguish warnings / errors more from other content

--- app/components/stop/stopError.tsx
+++ app/components/stop/stopError.tsx
@@ -1,8 +1,27 @@
+import { useRouter } from "next/navigation";
+
 export default function StopError({ error }: { error: string }) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/stops")
+  };
+
   return (
-    <div className="flex flex-row items-center gap-8 p-2 bg-yellow-500 text-black rounded-bl-3xl rounded-r-3xl">
-      <div className="text-2xl font-bold pl-8">!</div>
-      <p className="mx-2text-lg font-bold">{error}</p>
+    <div className="flex flex-col text-center gap-8">
+      <div className="flex flex-row items-center gap-8 p-2 bg-yellow-400 text-black">
+        <div className="text-2xl font-bold pl-8">!</div>
+        <p className="mx-2 text-lg font-bold">{error}</p>
+      </div>
+      <div>
+        <button
+          onClick={handleClick}
+          type="button" 
+          className="p-2 border-2 rounded-2xl font-mono"
+        >
+          ← Go back
+        </button>
+      </div>
     </div>
   );
 }
--- app/components/stop/stopWarning.tsx
+++ app/components/stop/stopWarning.tsx
@@ -1,6 +1,6 @@
 export default function StopWarning() {
   return (
-    <div className="flex flex-row items-center gap-8 p-2 bg-orange-600/90 text-black rounded-bl-3xl rounded-r-3xl">
+    <div className="flex flex-row items-center gap-8 p-2 bg-orange-500 text-black">
       <div className="text-2xl font-bold pl-8">!</div>
       <div className="mx-2">
         <p className="text-lg font-bold">
