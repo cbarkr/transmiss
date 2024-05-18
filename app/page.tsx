a17588a5fa94398e5b079453cf920192c74a00e3
cbarkr <cbarkr@gmail.com>
2024-05-18 13:39:47 -0700

    update font size/weight/style on home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="w-full">
       <div className="mt-64 flex flex-col gap-8 justify-center items-center text-center">
-        <h1 className="text-4xl font-bold">Transit missing the mark?</h1>
+        <h1 className="text-2xl">Transit missing the mark?</h1>
         <a
           href="/stops"
-          className="font-bold text-4xl text-blue-400 hover:underline underline-offset-4"
+          className="text-2xl font-mono text-blue-400 hover:underline underline-offset-4"
         >
           Report it →
         </a>
